330590a862bb96811c90cf000b061ef6a63deb02
Andras Tantos <andras@tantosonline.com>
2022-01-18 10:46:56 -0800

    Hello test and it's cohorts.

---
+++ tests/c/hello.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void x(void)
+{
+}
+
+int main(void)
+{
+    //const char *msg = "Hello world\n";
+    //write(1, msg, strlen(msg));
+    x();
+    printf("Hello world\n");
+    return 0;
+}
+
+long
+__modsi3 (long a, long b)
+{
+    return 0;
+}
