c8939245fc4a5046b09bb5c240488bbf712d3e65
Andras Tantos <andras@tantosonline.com>
2022-02-11 07:49:12 -0800

    Removed tests for now

--- tests/c/add_overflow_test.c
+++
@@ -1,5 +0,0 @@
-long long foo (long long a, long long *w)
-{
-  return __builtin_add_overflow (a, a, w);
-  //return __builtin_sadd_overflow(a, a, &w);
-}
--- tests/c/sh_test.c
+++
@@ -1,6 +0,0 @@
-int main(void)
-{
-  int a = 0xef;
-  int b = a >> 4;
-  return b;
-}
