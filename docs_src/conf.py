a373b0413a0d5cf5d4ec7960b0473421b6e5e7d7
Andras Tantos <andras@tantosonline.com>
2023-12-07 10:25:54 -0800

    Checking in HTML documentation.

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -64,6 +64,8 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+# Disable copying source over to build
+html_copy_source = False
 
 source_suffix = {
     '.rst': 'restructuredtext',
